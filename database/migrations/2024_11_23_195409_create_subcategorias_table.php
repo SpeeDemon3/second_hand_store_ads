330a5f55dd94eb2534caa936f543b7a071715cc7
Antonio Ruiz Benito <3speedemon3@gmail.com>
2024-11-23 21:31:13 +0100

    Add new migrations

---
+++ database/migrations/2024_11_23_195409_create_subcategorias_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use function Livewire\on;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subcategorias', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 105)->unique();
+            $table->text('descripcion');
+            $table->unsignedBigInteger('categoria_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subcategorias');
+    }
+};
---
+++ database/migrations/2024_11_23_202040_create_anuncios__table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('anuncios_', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->text('description')->nullable();
+            $table->text('imagen')->nullable();
+            $table->decimal('precio', 10, 2)->default(0);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('subcategoria_id');
+            $table->string('telefono');
+            $table->foreign('subcategoria_id')->references('id')->on('subcategorias');
+            $table->unsignedBigInteger('estado_id');
+            $table->foreign('estado_id')->references('id')->on('estados');
+            $table->string('provincia', 2);
+            $table->string('cod_postal', 5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('anuncios_');
+    }
+};
